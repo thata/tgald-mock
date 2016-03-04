0abc27ba294402de946b17e9741b5da9e10ccd81
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2016-03-04 15:27:17 +0900

    高さを調節するiframeのIDを渡せるようににした
    他のStanzaと共存できるよう、postMessageの引数へ高さを調節したいiframeのidを追加した。

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var iframe_id = req.query.iframe_id
   var n = Math.floor(Math.random() * 20) + 1
-  res.render('index', { title: 'Express', n: n });
+  res.render('index', { title: 'Express', n: n, iframe_id: iframe_id });
 });
 
 module.exports = router;
